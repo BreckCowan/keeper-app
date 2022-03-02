699ca4221f62566fe6da2e8c0c70e1f7fd51d7f0
Breck Cowan <breckcowan@gmail.com>
2022-03-02 17:49:32 -0600

    init commit

---
+++ src/components/Note.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Note() {
+  return (
+    <div className="note">
+        <h1>this is the title</h1>
+        <p>Sit enim est exercitation aliquip.</p>
+
+    </div>
+  )
+}
+
+export default Note;
